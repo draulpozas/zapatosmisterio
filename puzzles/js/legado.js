cdb27c024b1093a6ee08259ab9f0b52332492de9
Diego Pozas <draulpozas@gmail.com>
2020-04-21 02:14:22 +0200

    Third puzzle is now done

---
+++ puzzles/js/legado.js
@@ -0,0 +1,10 @@
+const sol = 'diamantes';
+
+function check() {
+    let input = document.getElementById('input').value;
+    input = input.trim().toLowerCase();
+    if (input === sol) {
+        window.alert('¡Superado!\nFragmento obtenido: "(70-81)los catando."');
+        document.body.innerHTML = `<h1><a href="tendencias.html">Habrá que seguir, ¿no?</a></h1>`;
+    }
+}
