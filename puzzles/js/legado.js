32b5c4433284826682fd14f63a2a41f4eee6c588
Diego Pozas <draulpozas@gmail.com>
2020-04-24 20:55:40 +0200

    Added listener for submitting pressing Enter. Also, added a warning sign that appears when the user enters an invalid answer.

--- puzzles/js/legado.js
+++ puzzles/js/legado.js
@@ -4,7 +4,18 @@ function check() {
     let input = document.getElementById('input').value;
     input = input.trim().toLowerCase();
     if (input === sol) {
+        document.getElementById('incorrect').classList.add('transparent');
         window.alert('¡Superado!\nFragmento obtenido: "(69-81)alos catando."');
         document.body.innerHTML = `<h1><a href="tendencias.html">Habrá que seguir, ¿no?</a></h1>`;
+    } else {
+        document.getElementById('incorrect').classList.remove('transparent');
     }
+}
+
+window.onload = function() {
+    document.getElementById('input').onkeyup = function(ev) {
+        if (ev.key == 'Enter') {
+            check();
+        }
+    };
 }
